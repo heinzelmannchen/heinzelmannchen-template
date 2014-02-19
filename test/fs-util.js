71f9425c5155b6a763f2839a8bf419ee08eebef7
Christoph Hermann <schtoeffel@gmail.com>
2014-02-19 15:56:35 +0100

    adds missing testcases

--- test/fs-util.js
+++ test/fs-util.js
@@ -3,36 +3,57 @@ var fsUtil = require('../lib/fs-util'),
     mockFs = require('mock-fs');
 
 describe('lib/fs-util', function() {
-    beforeEach(function() {
-        mockFs({
-            'foo': {
-                'bar.tpl': 'hello Edi'
-            }
+    describe('#readFileOrReturnData', function() {
+        beforeEach(function() {
+            mockFs({
+                'foo': {
+                    'bar.tpl': 'hello Edi'
+                }
+            });
+        });
+
+        it('should read "hello Edi"', function(done) {
+            fsUtil.readFileOrReturnData('foo/bar.tpl')
+                .then(function(data) {
+                    data.should.equal('hello Edi');
+                    done();
+                });
+        });
+
+        it('should throw an error if file doesn\'t exist', function(done) {
+            fsUtil.readFileOrReturnData('not/existing.tpl')
+                .fail(function(error) {
+                    error.should.be.ok;
+                    done();
+                });
+        });
+
+        it('should return the object if not a file and typeof object', function(done) {
+            fsUtil.readFileOrReturnData({
+                foo: 'bar'
+            }).then(function(obj) {
+                obj.should.have.property('foo');
+                done();
+            });
         });
     });
-
-    it('should read "hello Edi"', function(done) {
-        fsUtil.readFileOrReturnData('foo/bar.tpl')
-            .then(function(data) {
-                data.should.equal('hello Edi');
-                done();
+    describe('#isFile', function() {
+        beforeEach(function() {
+            mockFs({
+                'foo': {
+                    'bar.tpl': 'hello Edi'
+                }
             });
-    });
+        });
 
-    it('should throw an error if file doesn\'t exist', function(done) {
-        fsUtil.readFileOrReturnData('not/existing.tpl')
-            .fail(function(error) {
-                error.should.be.ok;
-                done();
-            });
-    });
+        it('should return true if it is a file', function() {
+            fsUtil.isFile('foo/bar.tpl').should.be.ok;
+        });
 
-    it('should return the object if not a file and typeof object', function(done) {
-        fsUtil.readFileOrReturnData({
-            foo: 'bar'
-        }).then(function(obj) {
-            obj.should.have.property('foo');
-            done();
+        it('should return false if it is not a file', function() {
+            fsUtil.isFile('not/existing.tpl').should.be.not.ok;
+            fsUtil.isFile(1).should.be.not.ok;
+            fsUtil.isFile().should.be.not.ok;
         });
     });
 });
