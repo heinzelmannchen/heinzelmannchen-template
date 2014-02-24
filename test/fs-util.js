0eb66b78b7053c401fe429168ee2ad9343fb6083
Christoph Hermann <schtoeffel@gmail.com>
2014-02-24 11:13:39 +0100

    refactored fs-util tests to match promise pattern

--- test/fs-util.js
+++ test/fs-util.js
@@ -1,5 +1,8 @@
-var fsUtil = require('../lib/fs-util'),
-    should = require('should'),
+var chai = require("chai"),
+    chaiAsPromised = require("chai-as-promised"),
+    should = chai.Should(),
+    mochaAsPromised = require("mocha-as-promised")(),
+    fsUtil = require('../lib/fs-util'),
     mockFs = require('mock-fs');
 
 describe('lib/fs-util', function() {
@@ -8,44 +11,29 @@ describe('lib/fs-util', function() {
             mockFs({
                 'foo': {
                     'bar.tpl': 'hello Edi',
-                    'no.one' : {
+                    'no.one': {
                         mode: '000'
                     }
                 }
             });
         });
 
-        it('should read "hello Edi"', function(done) {
-            fsUtil.readFileOrReturnData('foo/bar.tpl')
-                .then(function(data) {
-                    data.should.equal('hello Edi');
-                    done();
-                });
+        it('should read "hello Edi"', function() {
+            return fsUtil.readFileOrReturnData('foo/bar.tpl').should.become('hello Edi');
         });
 
-        it('should throw an error if file doesn\'t exist', function(done) {
-            fsUtil.readFileOrReturnData('not/existing.tpl')
-                .fail(function(error) {
-                    error.should.be.ok;
-                    done();
-                });
+        it('should throw an error if file doesn\'t exist', function() {
+            return fsUtil.readFileOrReturnData('not/existing.tpl').should.be.rejected;
         });
 
-        it('should fail if no permission', function(done) {
-            fsUtil.readFileOrReturnData('foo/no.one')
-                .fail(function(error) {
-                    error.should.be.ok;
-                    done();
-                });
+        it('should fail if no permission', function() {
+            return fsUtil.readFileOrReturnData('foo/no.one').should.be.rejected;
         });
 
-        it('should return the object if not a file and typeof object', function(done) {
-            fsUtil.readFileOrReturnData({
+        it('should return the object if not a file and typeof object', function() {
+            return fsUtil.readFileOrReturnData({
                 foo: 'bar'
-            }).then(function(obj) {
-                obj.should.have.property('foo');
-                done();
-            });
+            }).should.eventually.have.property('foo');
         });
     });
 
@@ -56,22 +44,15 @@ describe('lib/fs-util', function() {
             });
         });
 
-        it('should create a file', function(done) {
-            fsUtil.createFile('foo/newFile.x', 'content')
+        it('should create a file', function() {
+            return fsUtil.createFile('foo/newFile.x', 'content')
                 .then(function() {
                     return fsUtil.readFileOrReturnData('foo/newFile.x');
-                }).then(function(readContent) {
-                    readContent.should.equal('content');
-                    done();
-                });
+                }).should.become('content');
         });
 
-        it('should file if path doesn\'t exist', function(done) {
-            fsUtil.createFile('foo/bar/newFile.x', 'content')
-                .catch(function(error) {
-                    error.should.be.ok;
-                    done();
-                });
+        it('should file if path doesn\'t exist', function() {
+            return fsUtil.createFile('foo/bar/newFile.x', 'content').should.be.rejected;
         });
     });
 
@@ -82,21 +63,12 @@ describe('lib/fs-util', function() {
             });
         });
 
-        it('should return true if paths exists', function(done) {
-            fsUtil.pathExists('foo/bar/test')
-                .then(function(exists) {
-                    exists.should.equal(true);
-                    done();
-                });
+        it('should return true if paths exists', function() {
+            return fsUtil.pathExists('foo/bar/test').should.become(true);
         });
 
-        it('should return false if path doesn\'t exst', function(done) {
-            fsUtil.pathExists('foo/secret/bar')
-                .then(function(exists) {
-                    exists.should.equal(false);
-                    done();
-                });
+        it('should return false if path doesn\'t exst', function() {
+            return fsUtil.pathExists('foo/secret/bar').should.become(false);
         });
     });
-
 });
